c097d64a3c25236c1c220c6d07a20d2eb05cf945
Steven Maude <git@stevenmaude.co.uk>
2023-07-21 17:46:25 +0000

    Fix poppler-22.04.0 build
    See https://gitlab.freedesktop.org/poppler/poppler/-/commit/451acd97906681da4b385c8bb52a5dc971abb7fc

--- src/main.cpp
+++ src/main.cpp
@@ -523,13 +523,13 @@ BaseStream *open_file(const std::string filename) {
   // Original
   // GooString goo_filename(filename.c_str());
   // auto file = GooFile::open(&goo_filename);
-  auto file = GooFile::open(filename);
+  std::unique_ptr<GooFile> file = GooFile::open(filename);
   if (file == NULL) {
     std::cerr << "Failed to open " << filename << std::endl;
     exit(5);
   }
   Object obj;
-  return new FileStream(file, 0, false, file->size(), Object(objNull));
+  return new FileStream(file.release(), 0, false, file->size(), Object(objNull));
 }
 
 std::string parse_page_range(std::string value, Options *options) {
